fee84490c79f7f697dc9484ffb1bd3ab7b45ad4b
kimbogyu <bg0820@naver.com>
2019-05-11 20:19:47 +0900

    IngredientController 추가

--- GLIT/src/Controller/IngredientController.java
+++ GLIT/src/Controller/IngredientController.java
@@ -3,18 +3,10 @@ package Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import Model.ProfileContextMenu;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 public class IngredientController implements Initializable {
 	@FXML
