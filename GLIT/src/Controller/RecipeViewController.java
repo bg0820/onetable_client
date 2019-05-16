6f14f8a91267c7aa8fa63b7ffe3ad6b364a3cee3
이아현 <uiyn@naver.com>
2019-05-16 16:43:21 +0900

    ㅅㅈ
    ㅅㅈ

--- GLIT/src/Controller/RecipeViewController.java
+++ GLIT/src/Controller/RecipeViewController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import Model.ProfileContextMenu;
 //import Model.RecipeListItem;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,91 +15,64 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class RecipeViewController implements Initializable{
 
 	@FXML
-	private FlowPane listItemArea;
+	private ComboBox<String> selectMenu;
 	
-	@FXML
-	private ComboBox<String> selectFind;
-	@FXML 
-	private ComboBox<String> selectArray;
-	@FXML
-	private ComboBox<String> selectPrice;
+	ObservableList<String> menuList = FXCollections.observableArrayList("bg0820", "마이페이지", "즐겨찾기", "로그아웃");
 
-	ObservableList<String> findList = FXCollections.observableArrayList("전체",
-			"작성자명",
-			"레시피명",
-			"재료명");
-	
-	ObservableList<String> arrayList = FXCollections.observableArrayList("정확도순",
-			"최신순",
-			"평점순",
-			"가격순"); 
-	
-	ObservableList<String> priceList = FXCollections.observableArrayList("1만원 이하",
-			"1만원대",
-			"2만원대",
-			"3만원대",
-			"4만원대",
-			"5만원대"); 
-	
-	
-	/*public void listItemAdd(ActionEvent evenet)
-	{
-		RecipeListItem recipeListItem = new RecipeListItem(290, 350);
-		listItemArea.getChildren().add(recipeListItem);
-	}*/
+	// private boolean isIDDuplicate = true;
 
+	public void selectMenuChange(ActionEvent event) {
+
+		if (selectMenu.getValue().equals("마이페이지")) {
+
+		} else if (selectMenu.getValue().equals("즐겨찾기")) {
+
+		} else if (selectMenu.getValue().equals("로그아웃")) {
+
+		}
+
+	}
 	
 @Override
 public void initialize(URL location, ResourceBundle resources) {
 	// TODO Auto-generated method stub
+
+
 	
-	selectPrice.setManaged(false);
-	selectPrice.setVisible(false);
-	
-	selectFind.setItems(findList);
-	selectFind.getSelectionModel().select("전체");
-	
-	selectArray.setItems(arrayList);
-	//selectArray.getSelectionModel().select("전체");
-	
-	selectPrice.setItems(priceList);
-	//selectFind.getSelectionModel().select("전체");
 	
 }
+	/*
+	 * public void listItemAdd(ActionEvent evenet) { RecipeListItem recipeListItem =
+	 * new RecipeListItem(290, 350); listItemArea.getChildren().add(recipeListItem);
+	 * }
+	 */
 
-public void priceBtn(ActionEvent event) {
+	public void HomeBtn(ActionEvent event) throws IOException {
+		// 창 전환
+		Scene registerScene = new Scene(FXMLLoader.load(getClass().getResource("/View/LoginView.fxml")));
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(registerScene);
 
-	if (selectArray.getValue().equals("가격순")) {
-		selectPrice.setManaged(true);
-		selectPrice.setVisible(true);
-		
-	}
-		else {
-			selectPrice.setVisible(false);
-			selectPrice.setManaged(false);
-		}
 	}
 
+	public void IngredientBtn(ActionEvent event) throws IOException {
+		// 창 전환
+		Scene registerScene = new Scene(FXMLLoader.load(getClass().getResource("/View/IngredientView.fxml")));
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(registerScene);
 
-public void HomeBtn(ActionEvent event) throws IOException {
-	// 창 전환
-	Scene registerScene = new Scene(FXMLLoader.load(getClass().getResource("/View/LoginView.fxml")));
-	Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-	window.setScene(registerScene);
+	}
 
-}
+	public void contextMenu(MouseEvent event) {
+		ProfileContextMenu pcm = new ProfileContextMenu();
+		pcm.showContextMenu(event);
+	}
 
-public void IngredientBtn(ActionEvent event) throws IOException {
-	// 창 전환
-	Scene registerScene = new Scene(FXMLLoader.load(getClass().getResource("/View/IngredientView.fxml")));
-	Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-	window.setScene(registerScene);
-
-}
 }
--- GLIT/src/Main/Main.java
+++ GLIT/src/Main/Main.java
@@ -25,10 +25,10 @@ public class Main extends Application {
 			Font f = Font.loadFont(getClass().getResourceAsStream("/Font/BMHANNA.ttf"), 14);
 			System.out.println(f);
   
-			URL url = getClass().getResource("/View/RecipeView.fxml");
+			URL url = getClass().getResource("/View/RecipeFind.fxml");
 			FXMLLoader fxmlLoader = new FXMLLoader();
-			fxmlLoader.setLocation(url); 
-			fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+			fxmlLoader.setLocation(url);  
+			fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory()); 
 			Parent root = (Parent)fxmlLoader.load(url.openStream());  
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
